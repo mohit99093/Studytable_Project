e3f7832950a2e20b82439dd717d267a70c9f04f7
amanpra123 <amanpra333@gmail.com>
2020-12-19 12:55:31 +0530

    Footer added

---
+++ client/src/containers/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Footer.scss'
+
+export default function Footer() {
+    return (
+        <footer>
+           <div className="foot" >
+               <div className="flex" >
+                   <div className="lists" >
+                       <div><a href="https://blog.cred.club">Blog</a></div>
+                       <div><a href="https://blog.cred.club">Career</a></div>
+                       <div><a href="https://blog.cred.club">Terms and Condition</a></div>
+                       <div><a href="https://blog.cred.club">UPI FAQ</a></div>
+                       <div><a href="https://blog.cred.club">Google API</a></div>
+                       <div><a href="https://blog.cred.club">Security</a></div>
+                       <div><a href="https://blog.cred.club">Priacy policy</a></div>
+                       <div><a href="https://blog.cred.club">Return & Refund policy</a></div>
+                       <div className="odd" ><a href="https://blog.cred.club">Complete security. no asterisks.</a></div>
+                       <p className="par" >CRED encrypts all data and transactions to ensure a completely secure experience for our members.</p>
+                       <img src="https://web-assets.cred.club/_next/assets/images/home-page/security-cert.png" alt="pic" />
+                   </div>
+                   <div className="lis" >
+                      <div><span>calculators</span><a href="/calculators/sip-calculator">SIP calculator</a><a href="/calculators/emi-calculator" >EMI calculator</a><a href="/calculators/ppf-calculator" >PPF calculator</a><a href="/calculators/home-loan-calculator">home loan calculator</a><a href="/calculators/car-loan-calculator" >car loan calculator</a></div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>
+                       <div><span>About Studytable</span>
+                          <div className="desc" >CRED is a members only credit card bill payment platform that rewards its members for clearing their credit card bills on time. CRED members get access to exclusive rewards and experiences from premier brands upon clearing their credit card bills on CRED.</div>
+                       </div>                   
+                   </div>
+               </div>
+               <div className="par odd" >copyright © 2020 Studytable Pvt Ltd.</div>
+           </div>
+        </footer>
+    )
+}
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from '../../components/Home/Main/Main'
+import Footer from '../Footer/Footer'
 
 export default function Home() {
     return (
         <div>
             <Main/>
+            <Footer/>
         </div>
     )
 }
