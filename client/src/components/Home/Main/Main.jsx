cbe87de5d392ed0a199571d9db9cb2fe69991b56
amanpra123 <amanpra333@gmail.com>
2021-01-07 01:19:03 +0530

    Blog Page UI with addition of cards

--- client/src/components/Home/Main/Main.jsx
+++ client/src/components/Home/Main/Main.jsx
@@ -1,106 +1,130 @@
-import React from 'react'
-import './Main.scss'
-import DehazeIcon from '@material-ui/icons/Dehaze';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
+import React from "react";
+import "./Main.scss";
+import DehazeIcon from "@material-ui/icons/Dehaze";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
 
 export default function Main() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
 
-   const [state, setState] = React.useState({
-      right: false,
-    });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-    const toggleDrawer = (anchor, open) => (event) => {
-      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-        return;
-      }
-  
-      setState({ ...state, [anchor]: open });
-    };
+    setState({ ...state, [anchor]: open });
+  };
 
-    const list = (anchor) => (
-      <div
-        className="drawer"
-        role="presentation"
-        onClick={toggleDrawer(anchor, false)}
-        onKeyDown={toggleDrawer(anchor, false)}
-      >
-        <List>
-          <ListItem style={{padding:'20px',fontWeight:'bold',fontSize:'20px',display:'flex',justifyContent:'center'}} >
-             STUDY<span style={{fontSize:'30px'}} >table</span>
-          </ListItem>
-          <Divider/>
-          <ListItem style={{padding:'20px',fontWeight:'bold'}} >
-             <a style={{color:'black', textDecoration:'none' }}  href="/student" >Students</a>
-          </ListItem>
-          <Divider/>
-          <ListItem style={{padding:'20px',fontWeight:'bold'}} >
-             <a style={{color:'black', textDecoration:'none' }} href="/teacher" >Teacher</a>
-          </ListItem>
-          <Divider/>
-          <ListItem style={{padding:'20px',fontWeight:'bold'}} >
-             <a style={{color:'black', textDecoration:'none' }} href="/coaching" >Coaching</a>
-          </ListItem>
-          <Divider/>
-          <ListItem style={{padding:'20px',fontWeight:'bold'}} >
-            <a style={{color:'black', textDecoration:'none' }} href="/school" >School</a>
-         </ListItem>
-         <Divider/>
-         <ListItem style={{padding:'20px',fontWeight:'bold'}} >
-           <a style={{color:'black', textDecoration:'none' }} href="/contact" >Contact Us</a>
+  const list = (anchor) => (
+    <div
+      className="drawer"
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem
+          style={{
+            padding: "20px",
+            fontWeight: "bold",
+            fontSize: "20px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          STUDY<span style={{ fontSize: "30px" }}>table</span>
         </ListItem>
-         <Divider/>
-        </List>
-      </div>
-    );
-  
-    return (
-        <section className="main" >
-            <div className="navbar" >                            
-               <div className="menu" >
-                   <div className="list" >
-                      <a href="/student">Student</a>
-                   </div>
-                   <div className="list" >
-                      <a href="/teacher">Teacher</a>
-                   </div>
-                   <div className="list" >
-                      <a href="/school">School</a>
-                   </div>
-                   <div className="list" >
-                      <a href="/coaching">Coaching</a>
-                   </div>
-                   <div className="list" >
-                      <a href="/contact">Contact Us</a>
-                   </div>
-                   <div className="list" >
-                      <a href="/">Blogs</a>
-                   </div>
-                   <div className="btns" >
-                         Download Studytable
-                   </div>
-               </div>
-                 
+        <Divider />
+        <ListItem style={{ padding: "20px", fontWeight: "bold" }}>
+          <a style={{ color: "black", textDecoration: "none" }} href="/student">
+            Students
+          </a>
+        </ListItem>
+        <Divider />
+        <ListItem style={{ padding: "20px", fontWeight: "bold" }}>
+          <a style={{ color: "black", textDecoration: "none" }} href="/teacher">
+            Teacher
+          </a>
+        </ListItem>
+        <Divider />
+        <ListItem style={{ padding: "20px", fontWeight: "bold" }}>
+          <a
+            style={{ color: "black", textDecoration: "none" }}
+            href="/coaching"
+          >
+            Coaching
+          </a>
+        </ListItem>
+        <Divider />
+        <ListItem style={{ padding: "20px", fontWeight: "bold" }}>
+          <a style={{ color: "black", textDecoration: "none" }} href="/school">
+            School
+          </a>
+        </ListItem>
+        <Divider />
+        <ListItem style={{ padding: "20px", fontWeight: "bold" }}>
+          <a style={{ color: "black", textDecoration: "none" }} href="/contact">
+            Contact Us
+          </a>
+        </ListItem>
+        <Divider />
+      </List>
+    </div>
+  );
+
+  return (
+    <section className="main">
+      <div className="navbar">
+        <div className="menu">
+          <div className="list">
+            <a href="/student">Student</a>
+          </div>
+          <div className="list">
+            <a href="/teacher">Teacher</a>
+          </div>
+          <div className="list">
+            <a href="/school">School</a>
+          </div>
+          <div className="list">
+            <a href="/coaching">Coaching</a>
+          </div>
+          <div className="list">
+            <a href="/contact">Contact Us</a>
+          </div>
+          <div className="list">
+            <a href="/blog">Blogs</a>
+          </div>
+          <div className="btns">Download Studytable</div>
+        </div>
+
         <React.Fragment key="right">
-        <div className="ic" onClick={toggleDrawer("right", true)} > <DehazeIcon/> </div> 
-        <Drawer anchor={"right"} open={state["right"]} onClose={toggleDrawer("right", false)}>
-          {list("right")}
-        </Drawer>
-      </React.Fragment>
-            </div>
-            <div className="heads">
-                <div className="reward">
-                   STUDY<span>table</span>
-                </div>
-                <div className="sub" >
-                    Care a smile
-                </div>
-            </div>
-            <div className="dwnld" >
-                   Download Studytable
-            </div>
-        </section>
-    )
+          <div className="ic" onClick={toggleDrawer("right", true)}>
+            {" "}
+            <DehazeIcon />{" "}
+          </div>
+          <Drawer
+            anchor={"right"}
+            open={state["right"]}
+            onClose={toggleDrawer("right", false)}
+          >
+            {list("right")}
+          </Drawer>
+        </React.Fragment>
+      </div>
+      <div className="heads">
+        <div className="reward">
+          STUDY<span>table</span>
+        </div>
+        <div className="sub">Care a smile</div>
+      </div>
+      <div className="dwnld">Download Studytable</div>
+    </section>
+  );
 }
--- client/src/containers/Blogs/Blogs.jsx
+++ client/src/containers/Blogs/Blogs.jsx
@@ -1,5 +1,85 @@
 import React from "react";
+import "./Blogs.scss";
+import Footer from "../Footer/Footer";
+import { Button } from "@material-ui/core";
 
 export default function Blogs() {
-  return <a href="/blogs/detail">Blogs</a>;
+  return (
+    <React.Fragment>
+      <div className="blogs">
+        <a href="/" className="logo">
+          STUDY<span>table</span>
+        </a>
+        <h1>Blogs</h1>
+        <div className="contain">
+          <div className="card">
+            <img src="https://bmmagazine.co.uk/wp-content/uploads/2017/01/blogging-e1484908296381.jpg" />
+            <h3> Heading </h3>
+            <p>
+              It is a moment of honour for me to be associated as the Head of
+              Advisory Committee of Entrepreneurship Cell, NITH. E-Cell is a
+              platform for discovering and amplifying your interest in the world
+              of ent
+            </p>
+            <a href="/blogs/detail">
+              <Button className="butns"> Read More </Button>
+            </a>
+          </div>
+          <div className="card">
+            <img src="https://bmmagazine.co.uk/wp-content/uploads/2017/01/blogging-e1484908296381.jpg" />
+            <h3> Heading </h3>
+            <p>
+              It is a moment of honour for me to be associated as the Head of
+              Advisory Committee of Entrepreneurship Cell, NITH. E-Cell is a
+              platform for discovering and amplifying your interest in the world
+              of ent
+            </p>
+            <a href="/blogs/detail">
+              <Button className="butns"> Read More </Button>
+            </a>
+          </div>
+          <div className="card">
+            <img src="https://bmmagazine.co.uk/wp-content/uploads/2017/01/blogging-e1484908296381.jpg" />
+            <h3> Heading </h3>
+            <p>
+              It is a moment of honour for me to be associated as the Head of
+              Advisory Committee of Entrepreneurship Cell, NITH. E-Cell is a
+              platform for discovering and amplifying your interest in the world
+              of ent
+            </p>
+            <a href="/blogs/detail">
+              <Button className="butns"> Read More </Button>
+            </a>
+          </div>
+          <div className="card">
+            <img src="https://bmmagazine.co.uk/wp-content/uploads/2017/01/blogging-e1484908296381.jpg" />
+            <h3> Heading </h3>
+            <p>
+              It is a moment of honour for me to be associated as the Head of
+              Advisory Committee of Entrepreneurship Cell, NITH. E-Cell is a
+              platform for discovering and amplifying your interest in the world
+              of ent
+            </p>
+            <a href="/blogs/detail">
+              <Button className="butns"> Read More </Button>
+            </a>
+          </div>
+          <div className="card">
+            <img src="https://bmmagazine.co.uk/wp-content/uploads/2017/01/blogging-e1484908296381.jpg" />
+            <h3> Heading </h3>
+            <p>
+              It is a moment of honour for me to be associated as the Head of
+              Advisory Committee of Entrepreneurship Cell, NITH. E-Cell is a
+              platform for discovering and amplifying your interest in the world
+              of ent
+            </p>
+            <a href="/blogs/detail">
+              <Button className="butns"> Read More </Button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 }
