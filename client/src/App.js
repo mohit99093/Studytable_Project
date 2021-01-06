c8fc0e6cc02b63f5a40039a3489d50c34a2b61ec
amanpra123 <amanpra333@gmail.com>
2021-01-06 13:01:27 +0530

    Blog page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import Home from './containers/Home/Home'
-import Student from './containers/Product/Student'
-import Teacher from './containers/Product/Teacher'
-import School from './containers/Product/School'
-import Coaching from './containers/Product/Coaching'
+import Home from "./containers/Home/Home";
+import Student from "./containers/Product/Student";
+import Teacher from "./containers/Product/Teacher";
+import School from "./containers/Product/School";
+import Coaching from "./containers/Product/Coaching";
 
-import {Route,Switch} from 'react-router-dom'
-import Contact from './containers/Contact/Contact'
+import { Route, Switch } from "react-router-dom";
+import Contact from "./containers/Contact/Contact";
+import Blogs from "./containers/Blogs/Blogs";
+import Detail from "./containers/Blogs/Details/Detail";
 
 function App() {
   return (
     <div>
       <Switch>
-           <Route path="/" exact component={Home} />
-           <Route path="/student" component={Student} />
-           <Route path="/teacher" component={Teacher} />
-           <Route path="/school" component={School} />
-           <Route path="/coaching" component={Coaching} />
-           <Route path="/contact" component={Contact}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/student" component={Student} />
+        <Route path="/teacher" component={Teacher} />
+        <Route path="/school" component={School} />
+        <Route path="/coaching" component={Coaching} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/blog" component={Blogs} />
+        <Route path="/blogs/detail" component={Detail} />
       </Switch>
     </div>
   );
